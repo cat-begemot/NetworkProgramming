b362ea90f452556da45ef8957e7290d4ab4b2e9e
Alex <oholenko@vector-software.com>
2019-08-21 12:23:59 +0300

    Added GetRateFromNbu method with XML support

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,13 +1,47 @@
 ﻿using System;
+using System.IO;
 using System.Net;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace ConsoleTest
 {
-    internal class Program
-    {
-        internal static void Main(string[] args)
-        {
-            
-        }
-    }
+	internal class Program
+	{
+		internal static void Main(string[] args)
+		{
+			Console.WriteLine(GetRateFromNbu());
+		}
+
+		public static string GetRateFromNbu(string currencyName = "USD", string date = "20190821")
+		{
+			var requestUri = new UriBuilder("https", "bank.gov.ua");
+			requestUri.Path = "NBUStatService/v1/statdirectory/exchange";
+			requestUri.Query = $"valcode={currencyName}&date={date}";
+
+			var webRequest = WebRequest.Create(requestUri.Uri);
+			var webResponseStream = webRequest.GetResponse().GetResponseStream();
+
+			if (webResponseStream != null)
+			{
+				using (var xmlReader = XmlReader.Create(webResponseStream))
+				{
+					xmlReader.MoveToContent();
+
+					while (xmlReader.Read())
+						switch (xmlReader.NodeType)
+						{
+							case XmlNodeType.Element:
+								if (xmlReader.Name == "rate")
+									return ((XElement)XElement.ReadFrom(xmlReader)).Value;
+
+								break;
+						}
+				}
+			}
+
+			return string.Empty;
+		}
+	}
 }
+
